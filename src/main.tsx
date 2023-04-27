c1da0ffac4315007e7345db43d39e62d1b17e839
Antonio Cristovam <antoniocristovam@outlook.com.br>
2023-04-27 19:39:53 -0300

    fix: refactoring routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes } from "react-router-dom";
-import Header from "./components/Header";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Routes />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
--- src/modules/Dashboard/pages/index.tsx
+++ src/modules/Dashboard/pages/index.tsx
@@ -1,28 +1,7 @@
-import { CardBody } from "reactstrap";
-import Chart from "react-apexcharts";
-
 const Dashboard = () => {
-  const options = {
-    series: [
-      {
-        data: [12, 98, 45, 45, 23, 56, 7, 45, 9, 34, 11, 12, 13],
-      },
-    ],
-    xaxis: {
-      categories: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-    },
-    chart: {
-      toolbar: {
-        show: false,
-      },
-    },
-  };
-
   return (
     <>
-      <CardBody md={1}>
-        <Chart options={options} series={options.series} type="bar" />
-      </CardBody>
+      <h1>OI GATA</h1>
     </>
   );
 };
