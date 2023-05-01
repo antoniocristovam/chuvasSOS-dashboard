7904235881d2ef3467e93afe8dc86a89ec3a77f6
antoniocristovam <antoniocristovam@outlook.com.br>
2023-05-01 19:49:43 -0300

    fix: error cors

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,7 +6,7 @@ CORS(app)
 
 @app.route("/chuva")
 def chuva():
-    return {"chuva" : ["chuva1, chuva2", "chuva3"]}
+    return {"chuva" : ["chuva1, chuva2", "chuva3", "chuva4", "chuva5"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
